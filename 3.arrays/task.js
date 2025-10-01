c2555fd70e504b684580e7c31015c804ab13126d
oleggio <oleg.naydenovIC@gmail.com>
2025-10-01 14:51:14 +0300

    add 3 array_task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((element) => arr1[element] === arr2[element] && arr1.length === arr2.length)
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users
+        .filter(user => user.gender === gender)
+        .map(user => user.age)
+        .reduce((acc, val, index, user) => acc + val / user.length, 0);
 }
