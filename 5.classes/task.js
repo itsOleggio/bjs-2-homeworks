470d1968b15f0395ab019c79d1be684123d17cae
oleggio <oleg.naydenovIC@gmail.com>
2025-10-04 12:41:47 +0300

    add task 5 width 1,2,3

---
+++ 5.classes/task.js
@@ -0,0 +1,239 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this._state = 100;
+        this.type = null;
+    }
+
+    set state(newState) {
+        if (newState < 0) {
+            this._state = 0;
+        } else if (newState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newState;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+);
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) this.books.push(book);
+    }
+
+    findBookBy(type, value) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i][type] === value) {
+                return this.books[i];
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                return this.books.splice(i, 1)[0];
+            }
+        }
+        return null;
+    }
+
+}
+
+
+// Тестовые данные
+
+/*
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
+
+const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+);
+
+console.log(picknick.author); //"Аркадий и Борис Стругацкие"
+picknick.state = 10;
+console.log(picknick.state); //10
+picknick.fix();
+console.log(picknick.state); //15
+
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+    new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); //null
+console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+*/
+
+
+// Task 3
+
+
+class Student {
+    constructor(name, gender, age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.subject = null;
+        this.marks = {};
+    }
+
+    setSubject(subjectName) {
+        this.subject = subjectName;
+    }
+
+    addMark(mark, subjectName = this.subject) {
+        if (mark < 2 || mark > 5) {
+            return;
+        }
+
+        if (!subjectName) {
+            return;
+        }
+
+        if (!this.marks[subjectName]) {
+            this.marks[subjectName] = [];
+        }
+
+        this.marks[subjectName].push(mark)
+    }
+
+    getAverage() {
+        const allMarks = Object.values(this.marks).flat();
+        if (allMarks.length === 0) {
+            return 0;
+        }
+        const sum = allMarks.reduce((acc, val) => acc + val, 0);
+        return sum / allMarks.length;
+    }
+
+    getAverageBySubject(subjectName) {
+        const marks = this.marks[subjectName];
+        if (!marks || marks.length === 0) {
+            return 0;
+        }
+        const sum = marks.reduce((acc, val) => acc + val, 0);
+        return sum / marks.length;
+    }
+
+
+    exclude(reason) {
+        this.marks = {};
+        this.excluded = reason;
+        this.subject = null;
+    }
+}
+
+
+const student = new Student("Олег Никифоров");
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
+student.getAverageBySubject("физика"); // Средний балл по предмету физика 4.5
+student.getAverageBySubject("биология"); // Вернёт 0, так как по такому предмету нет никаких оценок.
+student.getAverage(); // Средний балл по всем предметам 4.75
+
+console.log(student);
+
+const student2 = new Student("Артем Зайкин", "male", 20);
+student2.setSubject("математика");
+student2.addMark(4, "математика");
+student2.addMark(5, "математика");
+student2.addMark(6, "математика");
+student2.addMark(7, "математика");
+student2.addMark(8, "математика");
+student2.addMark(9, "математика");
+student2.addMark(10, "математика");
+student2.addMark(11, "математика");
+student2.exclude("Болен");
+
+console.log(student2);
