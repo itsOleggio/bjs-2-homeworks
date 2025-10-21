b290f87930eb1a92d346905738569be4a37e7c36
oleggio <oleg.naydenovIC@gmail.com>
2025-10-21 11:54:59 +0300

    add task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,62 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    const cache = new Map();
+    const maxCacheSize = 5;
+
+    return function(...args) {
+        const hash = args.join(',');
+
+        if (cache.has(hash)) {
+            return `Из кеша: ${cache.get(hash)}`;
+        }
+
+        const result = func(...args);
+        cache.set(hash, result);
+
+        if (cache.size > maxCacheSize) {
+            const firstKey = cache.keys().next().value;
+            cache.delete(firstKey);
+        }
+
+        return `Вычисляем: ${result}`;
+    };
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    let firstCall = true;
+    let lastArgs = null;
+
+    function wrapper(...args) {
+        wrapper.allCount++;
+
+        if (firstCall) {
+            func(...args);
+            wrapper.count++;
+            firstCall = false;
+        } else {
+            lastArgs = args;
+        }
+
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            firstCall = true;
+
+            if (lastArgs !== null) {
+                func(...lastArgs);
+                lastArgs = null;
+            }
+
+            timeoutId = null;
+        }, delay);
+    }
+
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
+    return wrapper;
 }
+
+
